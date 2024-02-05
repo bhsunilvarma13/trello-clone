5f964abe126c949391d0a11900aa6c205cfffdff
bhsunilvarma13 <mailme4loops@gmail.com>
2024-02-05 21:14:37 +0530

    Add favicon.ico and font.woff2, update .gitignore and site config, and create marketing layout and components

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Navbar } from "./_components/navbar";
+import { Footer } from "./_components/footer";
+
+function MarketingLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="bg-slate-100 h-full">
+      <Navbar />
+      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      <Footer />
+    </div>
+  );
+}
+
+export default MarketingLayout;
---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "Taskify",
+  description:
+    "Taskify is a simple task management app. It helps you to organize your tasks and get things done.",
+};
